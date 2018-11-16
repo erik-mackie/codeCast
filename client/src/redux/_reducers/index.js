634ec61d13a8c03e31d714eb8a5baf92dd9d99dd
Erik Mackie <erik@erik.workStation>
2018-11-15 18:18:43 -0800

    login/ register components added and refactored

--- client/src/redux/_reducers/index.js
+++ client/src/redux/_reducers/index.js
@@ -3,11 +3,19 @@ import { combineReducers } from 'redux'
 import { fileReducer } from './file.reducer.js';
 import { directoryReducer } from './directory.reducer.js';
 import { chatReducer } from './chat.reducer.js';
-import { streamsReducer } from './streams.reducer.js';
+import { alertReducer } from './alert.reducer.js';
+import { authenticationReducer } from './authentication.reducer.js';
+import { registrationReducer } from './registration.reducer.js';
+import { usersReducer } from './users.reducer.js';
+
 
 export const rootReducer = combineReducers({
   file: fileReducer,
   directory: directoryReducer,
   chat: chatReducer,
-  streams: streamsReducer
+  streams: streamsReducer,
+  userAlert: alertReducer,
+  authentication: authenticationReducer,
+  registration: registrationReducer,
+  users: usersReducer
 })
